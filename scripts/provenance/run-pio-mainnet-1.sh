5971ee6cd016386dfc2f0caac0f7bec122c41af3
Bobak <brashidnia@gmail.com>
2022-04-13 04:01:55 -0700

    Fix pio-mainnet-1 script. Still need to set env variables manually

--- scripts/provenance/run-pio-mainnet-1.sh
+++ scripts/provenance/run-pio-mainnet-1.sh
@@ -5,13 +5,13 @@
 export GO_PATH="/home/boba/.go"
 export PIO_MAINNET_HOME="/var/provenance/pio-mainnet-1"
 export DAEMON_NAME="provenanced"
-export DAEMON_HOME="${PIO_HOME}"
+export DAEMON_HOME="${PIO_MAINNET_HOME}"
 export DAEMON_ALLOW_DOWNLOAD_BINARIES="true"
 export DAEMON_RESTART_AFTER_UPGRADE="true"
 
 script -a -c '$GO_PATH/bin/cosmovisor start \
 --home $PIO_MAINNET_HOME \
---p2p.seeds TODO \
+--p2p.seeds 4bd2fb0ae5a123f1db325960836004f980ee09b4@seed-0.provenance.io:26656, 048b991204d7aac7209229cbe457f622eed96e5d@seed-1.provenance.io:26656 \
 --x-crisis-skip-assert-invariants \
 --minimum-gas-prices 1905nhash \
 --log_format json' \
